ea5a5fab50fcf04e571752c9473fd128a0250e2e
SimaxSaab <minorea@yandex.ru>
2020-11-03 08:15:04 +0200

    Fixed vh.

--- js/main.js
+++ js/main.js
@@ -15,4 +15,11 @@ jQuery(document).ready(function($) {
     track: true,
     close: function () {$(".ui-helper-hidden-accessible").remove();  }
   });
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
